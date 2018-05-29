ba8a48db223bc6409c99026bc659a5fc44e9fec0
Eka Rudianto <ekarudianto@yahoo.com>
2018-05-29 14:14:29 +0800

    Fix production string env

--- webpack.prod.js
+++ webpack.prod.js
@@ -39,7 +39,7 @@ module.exports = merge(baseConfig, {
       new ExtractTextPlugin('style.css'),
       new webpack.DefinePlugin({
         'process.env': {
-          'NODE_ENV': 'production'
+          'NODE_ENV': JSON.stringify('production')
         }
       }),
     ],

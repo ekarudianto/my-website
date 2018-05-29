e4da904757da9878368334c6cc48bc6f5dcb78fa
Eka Rudianto <ekarudianto@yahoo.com>
2018-05-29 19:31:45 +0800

    Add new packages

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,7 @@
 import appConfig from '@/config/appConfig.json';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import Typed from 'react-typed';
 import './About.scss';
 
 class About extends Component {
@@ -17,7 +18,18 @@ class About extends Component {
     return (
       <div className='site__col site__col--about'>
         <div className='site__content'>
-          <h2 className='title'>Eka Rudianto</h2>
+          <h2 className='title no-margin'>Eka Rudianto</h2>
+          <span className='line'></span>
+          <p>
+            <Typed 
+              strings={this.state.typedOptions.strings}
+              typeSpeed={this.state.typedOptions.typeSpeed}
+              backSpeed={this.state.typedOptions.backSpeed}
+              backDelay={this.state.typedOptions.backDelay}
+              loop={this.state.typedOptions.loop}
+              startDelay={this.state.typedOptions.startDelay}
+            />
+          </p>
         </div>
       </div>
     );

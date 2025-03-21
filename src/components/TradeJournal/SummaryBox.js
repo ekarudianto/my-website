4fa71995739238a541cd1fbb7164c543f7412ffa
ekarudianto <ekarudianto@yahoo.com>
2025-03-21 22:56:55 +0100

    Modify URL

--- src/components/TradeJournal/SummaryBox.js
+++ src/components/TradeJournal/SummaryBox.js
@@ -107,8 +107,8 @@ const SummaryBox = ({ rows }) => {
         newsImpactedTrade,
         overnightFee,
     }});
-    const WEB_APP_URL = 'https://murmuring-earth-80569-478a80600db5.herokuapp.com/macros/s/AKfycbyfCOnfsndoGXnkZKHut-IHChot5QGLVl-AQmrNB1HJyxnXd7DZP9HZBXL6-3wFQgsXgw/exec';
-    // const WEB_APP_URL = 'http://localhost:3001/macros/s/AKfycbyfCOnfsndoGXnkZKHut-IHChot5QGLVl-AQmrNB1HJyxnXd7DZP9HZBXL6-3wFQgsXgw/exec';
+    const WEB_APP_URL = 'https://murmuring-earth-80569-478a80600db5.herokuapp.com/macros/s/AKfycby-Q_6Cmbt1dYkM3PWC1m0_-ZzTYTG62yA6yEDnMG7x2hyc-PQ0iUWbHQRLwS_gHD5FKw/exec';
+    // const WEB_APP_URL = 'http://localhost:3001/macros/s/AKfycby-Q_6Cmbt1dYkM3PWC1m0_-ZzTYTG62yA6yEDnMG7x2hyc-PQ0iUWbHQRLwS_gHD5FKw/exec';
 
     try {
       const response = await fetch(`${WEB_APP_URL}?sheet=${getCurrentMonth()}`, {
